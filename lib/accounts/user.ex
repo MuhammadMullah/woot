065fefd23389e184ca208e75b3ea63fa8fa26692
MuhammadMullah <muhammadmullah16@gmail.com>
2020-12-12 17:38:22 +0300

    user migration, schema and test

---
+++ lib/accounts/user.ex
@@ -0,0 +1,23 @@
+defmodule Woot.Accounts.User do
+  @moduledoc """
+    User Schema
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @required_fields [:points]
+
+  schema "users" do
+    field :points, :integer, default: 0
+    timestamps()
+  end
+
+  @doc false
+  def changeset(attrs) do
+    %__MODULE__{}
+    |> cast(attrs, [:points])
+    |> validate_required(@required_fields)
+    |> validate_number(:points, greater_than_or_equal_to: 0, less_than_or_equal_to: 100)
+  end
+end
---
+++ test/lib/accounts/user_test.exs
@@ -0,0 +1,19 @@
+defmodule Woot.Accounts.UserTest do
+  use Woot.DataCase
+
+  alias Ecto.Changeset
+  alias Woot.Accounts.User
+
+  @valid_attrs %{points: 2}
+
+
+
+  describe "user changeset" do
+    test "success: returns a valid changeset with valid attributes" do
+      # testing whether is_integer(:points) == true that is already
+      # taken care by ecto so we won't bother test.
+      changeset = User.changeset(@valid_attrs)
+      assert %Changeset{valid?: true, changes: changes} = changeset
+    end
+  end
+end
